320718104f60bcc67abf5f00c43651469fe07eb9
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2024-01-09 15:17:17 +0530

    Update gas_spike.js

--- scripts/gas_spike.js
+++ scripts/gas_spike.js
@@ -16,7 +16,7 @@ async function call() {
     const max_round = 10;
     const recipient = "RECEIVER_ADDRESS";
 
-    const gas_resp = await fetch('https://gasstation-mumbai.matic.today/v2')
+    const gas_resp = await fetch('https://gasstation-testnet.polygon.technology/v2')
     const gas = await gas_resp.json();
     const gasPrice = parseInt((gas.fast.maxFee) * 1e9);
     // const gasPrice = parseInt(1 * 1e8); //only for testing
